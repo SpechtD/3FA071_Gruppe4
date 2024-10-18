1242d6498de7329d38e9ff9f67e7f07b14a07bb8
Sebastian Wirth <sebastianwirth05@gmail.com>
2024-10-18 13:38:26 +0200

    feat: CustomerDao with custructor and all Methods

--- src/main/java/dev/hv/Customer.java
+++ src/main/java/dev/hv/Customer.java
@@ -1,4 +1,73 @@
 package dev.hv;
+import java.time.LocalDate;
+import java.util.UUID;
+import dev.hv.model.Gender;
+import dev.hv.model.ICustomer;
 
-public class Customer {
+public class Customer implements ICustomer {
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private Gender gender;
+    private LocalDate BirthDate;
+
+    // Constructor
+
+
+    public Customer(UUID id, String firstName,String lastName, Gender gender, LocalDate BirthDate) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.gender = gender;
+        this.BirthDate = BirthDate;
+    }
+
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Override
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthDate() {
+        return BirthDate;
+    }
+
+    public void setBirthDate(LocalDate BirthDate) {
+        this.BirthDate = BirthDate;
+    }
+
+    @Override
+    public Gender getGender() {
+        return gender;
+    }
+
+    @Override
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
 }
--- src/main/java/dev/hv/dao/CustomerDao.java
+++ src/main/java/dev/hv/dao/CustomerDao.java
@@ -1,4 +1,132 @@
 package dev.hv.dao;
+import dev.hv.Customer;
+import dev.hv.dao.DbConnection;
+import dev.hv.model.Gender;
 
-public class CustomerDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.UUID;
+
+
+public class CustomerDao implements IDao<Customer> {
+
+   private Connection connection;
+
+   public CustomerDao(){
+       //TODO Insert Connection class method
+
+       this.connection = connection;
+
+   }
+
+    @Override
+    public void create(Customer customer) {
+        //String sql ="INSERT INTO Customer (UUID, firstName, lastName, birthDate, gender) VALUES (UUID, firstName, lastName, birthDate, gender);";
+
+        try (PreparedStatement statement = connection.prepareStatement("""
+              INSERT INTO Customer (UUID, firstName, lastName, birthDate, gender)
+              VALUES (?, ?, ?, ?, ? )
+              """)) {
+            statement.setObject(1, customer.getId());
+            statement.setString(2, customer.getFirstName());
+            statement.setString(3, customer.getLastName());
+            statement.setObject(4, customer.getBirthDate());
+            statement.setObject(5, customer.getGender());
+
+            int rowsInserted = statement.executeUpdate();
+
+            if (rowsInserted == 0) {
+                // Handle the case where no rows were updated
+                throw new RuntimeException("No Rows were inserted for your Input: Type or formating error");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("CustomerDao create Failure: " + e);
+        }
+
+
+    }
+
+    @Override
+    public Customer read(UUID id) {
+
+
+        try (PreparedStatement statement = connection.prepareStatement("""
+            SELECT *
+            FROM Customer
+            WHERE UUID = ?
+            """)) {
+
+            statement.setObject(1, id);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    // Assuming your Customer class has the following fields: firstName, lastName, birthDate, gender
+                    String firstName = resultSet.getString("firstName");
+                    String lastName = resultSet.getString("lastName");
+                    LocalDate birthDate = resultSet.getObject("birthDate", LocalDate.class);
+                    String genderString = resultSet.getString("gender");
+                    Gender gender = Gender.valueOf(genderString);
+
+                    // Create and return the Customer object
+                    return new Customer(id, firstName, lastName, gender, birthDate);
+                } else {
+                    // No customer found
+                    return null;
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("CustomerDao read failure: " + e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public void update(Customer customer) {
+        //String sql ="";
+
+        try (PreparedStatement statement = connection.prepareStatement("""
+              UPDATE Customer
+              SET firstName = ?, lastName = ?, birthDate = ?, gender = ?
+              WHERE CustomerID = ?
+              """)) {
+            statement.setString(1, customer.getFirstName());
+            statement.setString(2, customer.getLastName());
+            statement.setObject(3, customer.getBirthDate());
+            statement.setObject(4, customer.getGender());
+            statement.setObject(5, customer.getId());
+
+            int rowsUpdated = statement.executeUpdate();
+
+            if (rowsUpdated == 0) {
+                // Handle the case where no rows were updated
+                throw new RuntimeException("No customer found with the provided ID: " + customer.getId());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("CustomerDao update Failure: " + e);
+        }
+    }
+
+    @Override
+    public void delete(UUID id) {
+        //String sql ="DELETE FROM Customers WHERE CustomerName='Alfreds Futterkiste';";
+
+        try (PreparedStatement statement = connection.prepareStatement("""
+                DELETE * FROM Customer
+                WHERE CustomerID = ?
+                """)) {
+            statement.setObject(1, id);
+
+            int rowsUpdated = statement.executeUpdate();
+
+            if (rowsUpdated == 0) {
+                // Handle the case where no rows were updated
+                throw new RuntimeException("No customer found with the provided ID");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("CustomerDao delete Failure: " + e);
+        }
+    }
 }
