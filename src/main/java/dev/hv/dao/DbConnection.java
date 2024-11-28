f389f759aaa687adc98a2a4bba485bfc28ef1375
q540890 <amelie.roduner@bmw.de>
2024-11-28 11:43:29 +0100

    feat: changed Dbconnector to singlton, added createAlltables

--- src/main/java/dev/hv/dao/DbConnection.java
+++ src/main/java/dev/hv/dao/DbConnection.java
@@ -1,26 +1,34 @@
 package dev.hv.dao;
 
+import dev.hv.model.IDatabaseConnection;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 
-public class DbConnection {
+public class DbConnection implements IDatabaseConnection {
 
-    private String dbUrl;
-    private String dbUser;
-    private String dbPassword;
     private Connection connection;
 
-    public DbConnection() {
+    private static DbConnection INSTANCE;
+    private static final String DB_Properties = "/db.properties";
+
+    private DbConnection() {
         getLoginProperties();
 
     }
 
-    private static final String DB_Properties = "/db.properties";
+    public static DbConnection getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new DbConnection();
+        }
+        return INSTANCE;
+    }
 
-    public void getLoginProperties() {
+    public static Properties getLoginProperties() {
 
         final String user_home = System.getProperty("user.home");
 
@@ -30,29 +38,69 @@ public class DbConnection {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+        return prop;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    @Override
+    public IDatabaseConnection openConnection(Properties properties) {
+        //if (connection == null || connection.isClosed()) {
 
         String username = System.getProperty("user.name");
 
-        dbUrl = prop.getProperty(username + ".db.url");
-        dbUser = prop.getProperty(username + ".db.user");
-        dbPassword = prop.getProperty(username + ".db.psw", "");
-
-        System.out.println(dbUser);
-        System.out.println(dbPassword);
-        System.out.println(dbUrl);
+        String dbUrl = properties.getProperty(username + ".db.url");
+        String dbUser = properties.getProperty(username + ".db.user");
+        String dbPassword = properties.getProperty(username + ".db.psw", "");
 
         if (dbUrl == null || dbUser == null) {
             throw new RuntimeException("Database credentials not found for user: " + username);
         }
 
+        try {
+            connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        // }
+        return this;
     }
 
-    public Connection connectToDatabase() throws SQLException {
-        //if (connection == null || connection.isClosed()) {
-        connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword);
-        // }
-        System.out.println(connection.isValid(10));
-        return connection;
+    @Override
+    public void createAllTables() {
+        String table = "CREATE TABLE IF NOT EXISTS Customer " +
+                "(id UUID PRIMARY KEY NOT NULL, " +
+                "firstName VARCHAR(50) NOT NULL, " +
+                "lastName VARCHAR(50) NOT NULL, " +
+                "birthDate, " +
+                "gender CHAR(1))" +
+
+                "CREATE TABLE IF NOT EXISTS Reading " +
+                "(id UUID PRIMARY KEY NOT NULL, " +
+                "comment VARCHAR(255) NOT NULL, " +
+                "customer UUID NOT NULL, " +
+                "dateOfReading DATE DEFAULT CURRENT_DATE, " +
+                "kindOfMeter VARCHAR(50) NOT NULL," +
+                "meterCount DOUBLE," +
+                "substitute BIT," +
+                "type VARCHAR(50))";
+        try (Statement stmt = connection.createStatement()) {
+            stmt.executeUpdate(table);
+        } catch (SQLException e) {
+            throw new RuntimeException("createAllTables wasn't successful: " + e);
+        }
+    }
+
+    @Override
+    public void truncateAllTables() {
+
+    }
+
+    @Override
+    public void removeAllTables() {
+
     }
 
     public void closeConnection() {
@@ -63,4 +111,5 @@ public class DbConnection {
         }
     }
 }
+
 
--- src/main/java/dev/hv/dao/Main.java
+++ src/main/java/dev/hv/dao/Main.java
@@ -7,15 +7,8 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) {
         // Instanz von DbConnection erstellen
-        DbConnection dbConnection = new DbConnection();
+        DbConnection.getInstance().openConnection(DbConnection.getLoginProperties());
 
-        try {
-            // Lade und drucke die Datenbankverbindungseigenschaften
-            dbConnection.connectToDatabase();
-        } catch (SQLException e) {
-            System.err.println("Fehler beim Laden der Eigenschaften: " + e.getMessage());
-        } finally {
-            dbConnection.closeConnection();
-        }
+        DbConnection.getInstance().closeConnection();
     }
 }
