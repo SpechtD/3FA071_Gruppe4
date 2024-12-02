85fc237c3f7a5cd48fcb944ce9a67611091737c2
Michaela Kappelmeier (PT-301) <michaela.kappelmeier@bmw.de>
2024-12-02 10:57:43 +0100

    ci: Implementation of Dao in Reading class
    closes #11

--- src/main/java/dev/hv/Reading.java
+++ src/main/java/dev/hv/Reading.java
@@ -3,6 +3,7 @@ package dev.hv;
 import dev.hv.model.ICustomer;
 import dev.hv.model.IReading;
 import dev.hv.model.KindOfMeter;
+import dev.hv.dao.ReadingDao;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -17,6 +18,7 @@ public class Reading implements IReading {
     private double meterCount;
     private String meterId;
     private boolean substitute;
+    ReadingDao dao = new ReadingDao();
 
 
     public Reading(UUID id, String comment, ICustomer customer, LocalDate dateOfReading, KindOfMeter kindOfMeter, double meterCount, String meterId, boolean substitute) {
@@ -28,89 +30,92 @@ public class Reading implements IReading {
         this.meterCount = meterCount;
         this.meterId = meterId;
         this.substitute = substitute;
+        dao.create(this);
     }
 
     @Override
     public void setId(UUID id) {
         this.id=id;
+        dao.update(this);
     }
 
     @Override
     public void setComment(String comment) {
         this.comment=comment;
+        dao.update(this);
     }
 
     @Override
     public void setCustomer(ICustomer customer) {
         this.customer=customer;
+        dao.update(this);
     }
 
     @Override
     public void setDateOfReading(LocalDate dateOfReading) {
         this.dateOfReading=dateOfReading;
+        dao.update(this);
     }
 
     @Override
     public void setKindOfMeter(KindOfMeter kindOfMeter) {
         this.kindOfMeter=kindOfMeter;
+        dao.update(this);
 
     }
 
     @Override
     public void setMeterCount(Double meterCount) {
         this.meterCount=meterCount;
+        dao.update(this);
     }
 
     @Override
     public void setMeterId(String meterId) {
         this.meterId=meterId;
-
+        dao.update(this);
     }
 
     @Override
     public void setSubstitute(Boolean substitute) {
         this.substitute=substitute;
-
+        dao.update(this);
     }
 
     @Override
-    public UUID getId() {
-        return id;
-    }
+    public UUID getId() { return dao.read(this.id).id; }
 
     @Override
-    public String getComment() {
-        return comment;
-    }
+    public String getComment() { return dao.read(this.id).comment; }
 
     @Override
     public ICustomer getCustomer() {
-        return customer;
+        return dao.read(this.id).customer;
     }
 
     @Override
     public LocalDate getDateOfReading() {
-        return dateOfReading;
+        return dao.read(this.id).dateOfReading;
     }
 
     @Override
     public KindOfMeter getKindOfMeter() {
-        return kindOfMeter;
+        return dao.read(this.id).kindOfMeter;
     }
 
     @Override
     public Double getMeterCount() {
-        return meterCount;
+        return dao.read(this.id).meterCount;
     }
 
     @Override
     public String getMeterId() {
-        return meterId;
+        return dao.read(this.id).meterId;
     }
 
     @Override
     public Boolean getSubstitute() {
-        return substitute;
+        return dao.read(this.id).substitute;
     }
 
     @Override
