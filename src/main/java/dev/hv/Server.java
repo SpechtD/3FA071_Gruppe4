962a4655f4be1bb3726940849ae464fbf9828281
Michaela Kappelmeier (PT-301) <michaela.kappelmeier@bmw.de>
2025-01-20 13:09:59 +0100

    feature: add Server class and dependencies functions: -startServer(url) -stopServer()
    closes #20

---
+++ src/main/java/dev/hv/Server.java
@@ -0,0 +1,25 @@
+package dev.hv;
+
+import com.sun.net.httpserver.HttpServer;
+import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
+public class Server {
+    static HttpServer server;
+
+    static void startServer(String url){
+        final String pack = "dev.hv.resource";
+        System.out.println("Start server");
+        System.out.println(url);
+        final ResourceConfig rc = new ResourceConfig().packages(pack);
+        server = JdkHttpServerFactory.createHttpServer(URI.create(url), rc);
+    }
+
+    static void stopServer(){
+        server.stop(0);
+    }
+
+}
+
