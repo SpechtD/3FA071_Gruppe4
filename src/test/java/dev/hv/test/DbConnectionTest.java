031657dc5d37be210e906b0e390b98636193349f
Sebastian <sebastian.wirth@bmw.de>
2025-01-27 13:02:25 +0100

    feat: Db test inital commit

---
+++ src/test/java/dev/hv/test/DbConnectionTest.java
@@ -0,0 +1,40 @@
+package dev.hv.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DbConnectionTest {
+
+    @Test
+    void getInstance() {
+    }
+
+    @Test
+    void getLoginProperties() {
+    }
+
+    @Test
+    void getConnection() {
+    }
+
+    @Test
+    void openConnection() {
+    }
+
+    @Test
+    void createAllTables() {
+    }
+
+    @Test
+    void truncateAllTables() {
+    }
+
+    @Test
+    void removeAllTables() {
+    }
+
+    @Test
+    void closeConnection() {
+    }
+}
