e8de3424502225d9e3c639109d27ccbe03a264cc
q531351 <david.specht@bmw.de>
2025-01-28 22:30:02 +0100

    fix: change print to logger

--- src/main/java/dev/hv/Server.java
+++ src/main/java/dev/hv/Server.java
@@ -1,6 +1,8 @@
 package dev.hv;
 
 import com.sun.net.httpserver.HttpServer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -9,11 +11,12 @@ import java.net.URI;
 public class Server {
     static HttpServer server;
 
+    private static final Logger logger = LogManager.getLogger(Server.class);
+
+
     static void startServer(String url){
-        final String pack = "dev.hv.resource";
-        System.out.println("Start server");
-        System.out.println(url);
         final String pack = "dev.hv.endpoints";
+        logger.debug("Server started on: {}", url);
         final ResourceConfig rc = new ResourceConfig().packages(pack);
         server = JdkHttpServerFactory.createHttpServer(URI.create(url), rc);
     }
